2748950efe227fdb0dba03be8b480d69de4c6168
davidruizletgo <david.ruiz@letgo.com>
2022-02-04 17:11:12 +0100

    Adds empty fragments and navigation

--- app/src/main/java/com/ruizurraca/marveltest/presentation/MainActivity.kt
+++ app/src/main/java/com/ruizurraca/marveltest/presentation/MainActivity.kt
@@ -2,11 +2,48 @@ package com.ruizurraca.marveltest.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.ruizurraca.marveltest.R
+import com.ruizurraca.marveltest.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.ui.setupActionBarWithNavController
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            setupNavigation()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.homeFragment, R.id.aboutFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
---
+++ app/src/main/java/com/ruizurraca/marveltest/presentation/about/AboutFragment.kt
@@ -0,0 +1,23 @@
+package com.ruizurraca.marveltest.presentation.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ruizurraca.marveltest.databinding.FragmentAboutBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AboutFragment : Fragment() {
+    lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAboutBinding.inflate(inflater)
+        return binding.root
+    }
+}
