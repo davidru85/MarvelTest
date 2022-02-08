96fae451c60cd7dfd1c9f54c9683e3ad542b4af2
davidruizletgo <david.ruiz@letgo.com>
2022-02-08 17:24:46 +0100

    Adds detail module tests

---
+++ detail/src/test/java/com/ruizurraca/detail/presentation/DetailViewModelTest.kt
@@ -0,0 +1,42 @@
+package com.ruizurraca.detail.presentation
+
+
+import androidx.lifecycle.SavedStateHandle
+import com.ruizurraca.detail.domain.repository.MarvelCharacterDetailRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@ExperimentalCoroutinesApi
+class DetailViewModelTest {
+    @MockK
+    var savedStateHandle: SavedStateHandle = mockk(relaxed = true)
+
+    @MockK
+    var characterDetailRepository: MarvelCharacterDetailRepository = mockk(relaxed = true)
+
+    lateinit var viewModel: DetailViewModel
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        viewModel = buildViewModel()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3", "34"])
+    fun `viewmodel getCharacterDetail should call repo getCharacterDetail`(charId: String) {
+        every { savedStateHandle.get<String>(DetailViewModel.ID) } returns charId
+        viewModel.getCharacterDetail(charId)
+
+        coVerify { characterDetailRepository.getCharacterDetail(charId) }
+    }
+
+    private fun buildViewModel() = DetailViewModel(savedStateHandle, characterDetailRepository)
+}
